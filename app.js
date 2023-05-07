a33f250267ecca80352f3b710fd937af43cd8fd0
param-ninthu <ninthu1999@gmail.com>
2023-05-07 16:58:32 +0530

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+//load config
+dotenv.config({ path: "./config/config.env" });
+
+// call db
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
